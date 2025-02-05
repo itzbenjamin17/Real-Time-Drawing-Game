628dbd09360584b9d60c13ca19892ece7f651246
Yiu Ching Sheldon Yu <yiu.yu@student.manchester.ac.uk>
2025-02-05 16:24:40 +0000

    Drawing Screen (not started)

---
+++ src/Drawing.tsx
@@ -0,0 +1,12 @@
+import './components/Button.scss';
+import Button from "./components/Button";
+
+function App() {
+
+  return <div>
+    <Button onClick={() => console.log('hehehheheheheheh')}>hehehe</Button>
+    <div />
+  </div>;
+}
+
+export default App;
