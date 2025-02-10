16d918d1ce963d9a512f99b77c62d4e02c078402
Johan Jobi <johan.jobi@student.manchester.ac.uk>
2025-02-10 13:51:48 +0000

    added game settings page and leaderboard page

---
+++ ScribbleVerse/frontend/templates/frontend/leaderboard/main.js
@@ -0,0 +1,17 @@
+
+function goToLeaderboard() {
+    const playersData = {
+        "a": 120,
+        "b": 200,
+        "c": 150
+    };
+
+    // Save the dictionary in localStorage
+    localStorage.setItem('playersData', JSON.stringify(playersData));
+
+    window.location.href = "leaderboard.html";
+}
+
+function goToGameSettings(){
+    window.location.href = "gameSettings.html";
+}
