fe2156910bd38114d1927cf47b3ccc9598840a2e
Benjamin Adubofour <benjaminadubofourjunior@gmail.com>
2025-02-11 11:54:50 +0000

    Added js file for the index page

---
+++ ScribbleVerse/frontend/static/js/index.js
@@ -0,0 +1,14 @@
+function playGame() {
+    let username = document.getElementById('username').value;
+    if (username) {
+        alert('Playing as ' + username);
+        // start of the game logic 
+    } else {
+        alert('Please enter a username!');
+    }
+}
+
+function createRoom() {
+    alert('Create Room button clicked!');
+    // room creation logic 
+}
