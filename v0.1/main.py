5c15de81fb19fb6e4a43fc1fae28feba0e63aba1
Benjamin Adubofour <benjamin.adubofourjunior@student.manchester.ac.uk>
2025-02-21 23:37:48 +0000

    Added database models and api endpoints

--- v0.1/main.py
+++ v0.1/main.py
@@ -1,14 +1,22 @@
 # Use this to run the server:
 # flask --app main.py run --host=0.0.0.0 --port=5000
 
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 from flask_socketio import join_room, leave_room, send, SocketIO
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
 import random
 from string import ascii_uppercase
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "test"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
 socketio = SocketIO(app)
+api = Api(app)
+db = SQLAlchemy(app)
 
 rooms = {}
 
@@ -18,11 +26,161 @@ def generate_unique_code(Length):
         for _ in range(Length):
             code += random.choice(ascii_uppercase)
 
-        if code not in rooms:
+        if not Room.query.filter_by(code=code).first():
             break
 
     return code
 
+
+
+# ----------------- Database Models ----------------- #
+class Room(db.Model):
+    """
+    Room table:
+    - id | code | host | num_of_rounds | num_of_players | created_at
+    - Defines a one-to-many relationship with Player and Round tables.
+    - Players and Rounds are deleted when a Room is deleted.
+    - Can access the room a player is in using player = Player.query.first() then using player.room
+    - Can access the players in a room using room = Room.query.first() then using room.players
+    - Delete orphan is used to delete players and rounds when a room is deleted.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(6), unique=True, nullable=False)
+    host = db.Column(db.String(50), nullable=False)
+    num_of_rounds = db.Column(db.Integer, default=3)
+    num_of_players = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+
+    players = db.relationship('Player', backref='room', cascade="all, delete-orphan", lazy=True)
+    rounds = db.relationship('Round', backref='room', cascade="all, delete-orphan", lazy=True)
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    score = db.Column(db.Integer, default=0)
+    is_drawing = db.Column(db.Boolean, default=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+
+class Round(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(50), nullable=False)
+    round_number = db.Column(db.Integer, default=1)
+    is_over = db.Column(db.Boolean, default=False)
+    time_to_guess = db.Column(db.Integer, default=60)
+    current_time = db.Column(db.Integer, default=0)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+
+
+
+# ----------------- API Endpoints ----------------- #
+# Example use of the API:
+# - To get all the players in a room:
+#    fetch('/api/get-players/ABC123/')
+#       .then(response => {
+#           if (!response.ok) {
+#               throw new Error("Room not found");
+#           }
+#           return response.json();
+#        })
+#        .then(players => {
+#           console.log("Players in room:", players);
+#        })
+#        .catch(error => {
+#           console.error("Error:", error.message);
+#         });
+
+class RoomView(Resource):
+    '''
+    Returns a list of all rooms.
+    '''
+    def get(self):
+        rooms = Room.query.all()
+        return jsonify([{"id": room.id, "code": room.code, "host": room.host} for room in rooms])
+
+class ValidateRoomCode(Resource):
+    ''' 
+    Input: A room code
+    Returns {"valid": true}, 200 if room exists, else {"valid": false}, 404.
+    '''
+    def get(self, code):
+        room_exists = Room.query.filter_by(code=code).first() is not None
+        return {"valid": room_exists}, 200 if room_exists else 404
+
+class CreateRoom(Resource):
+    '''
+    Input: JSON with a host name ("host") and an optional ("num_of_rounds").
+    Posts a room in the database and returns the room's id and code, along with a 201 status code. (Meaning created)
+    If no num_of_rounds is provided, it defaults to 3.
+    '''
+    def post(self):
+        data = request.get_json()
+        room_code = generate_unique_code(6)
+        new_room = Room(code=room_code, host=data.get("host"), num_of_rounds=data.get("num_of_rounds", 3))
+        db.session.add(new_room)
+        db.session.commit()
+        return {"id": new_room.id, "code": new_room.code}, 201
+
+class GetRoom(Resource):
+    '''
+    Input: A room code
+    Returns the room's id, code, and host, along with a 200 status code.
+    If the room does not exist, returns {"error": "Room not found"}, 404.
+    '''
+    def get(self, code):
+        room = Room.query.filter_by(code=code).first()
+        if not room:
+            return {"error": "Room not found"}, 404
+        return {"id": room.id, "code": room.code, "host": room.host}, 200
+
+class AddPlayer(Resource):
+    '''
+    Input: a room code ("room") and a player name ("name").
+    Returns the player's id and a 201 status code. (Meaning created)
+    If the room does not exist, returns {"error": "Room not found"}, 404.
+    If the room is full, returns {"error": "Room is full"}, 400.
+    '''
+    def post(self):
+        data = request.get_json()
+        room_code = data.get("room")
+        room = Room.query.filter_by(code=room_code).first()
+        if not room:
+            return {"error": "Room not found"}, 404
+        if room.num_of_players >= 10:
+            return {"error": "Room is full"}, 400
+        
+        new_player = Player(name=data.get("name"), room_id=room.id)
+        room.num_of_players += 1
+        db.session.add(new_player)
+        db.session.commit()
+        return {"id": new_player.id}, 201
+
+class GetPlayersInRoom(Resource):
+    '''
+    Input: A room code
+    Returns a list of players in the room, along with a 200 status code.
+    If the room does not exist, returns {"error": "Room not found"}, 404.
+    '''
+    def get(self, room_code):
+        room = Room.query.filter_by(code=room_code).first()
+        
+        if not room:
+            return {"error": "Room not found"}, 404
+        
+        players = Player.query.filter_by(room_id=room.id).all()
+        players_data = [{"id": player.id, "name": player.name, "score": player.score} for player in players]      
+        return jsonify(players_data)
+
+# Register API endpoints with Flask-RESTful
+api.add_resource(RoomView, '/api/rooms/')
+api.add_resource(ValidateRoomCode, '/api/validate-room-code/<string:code>/')
+api.add_resource(CreateRoom, '/api/create-room/')
+api.add_resource(GetRoom, '/api/get-room/<string:code>/')
+api.add_resource(AddPlayer, '/api/add-player/')
+api.add_resource(GetPlayersInRoom, '/api/get-players-in-room/<string:room_code>/')
+
+
+
+# ----------------- Routes ----------------- #
 @app.route("/", methods = ["POST", "GET"])
 def index():
     session.clear()
@@ -118,6 +276,9 @@ def create_room():
 def join_room():
     return render_template("room.html", code=room, messages=rooms[room]["messages"])
 
+
+
+# ----------------- Socket Connections ----------------- #
 @socketio.on("message")
 def message(data):
     room = session.get("room")
@@ -131,7 +292,7 @@ def message(data):
     send(content, to=room)
     rooms[room]["messages"].append(content)
     print(f"{session.get('name')} said: {data['data']}")
-
+    
 @socketio.on("connect")
 def connect(auth):
     room = session.get("room")
