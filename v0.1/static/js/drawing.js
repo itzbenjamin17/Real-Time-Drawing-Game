088d99f7971908046e64c04e9c6dcb26a6ac1ca4
Benjamin Adubofour <benjamin.adubofourjunior@student.manchester.ac.uk>
2025-03-07 20:14:47 +0000

    Fixed the dotted drawing on the canvas

--- v0.1/static/js/drawing.js
+++ v0.1/static/js/drawing.js
@@ -6,10 +6,15 @@ let mode = "pencil";
 let minutes = "3";
 let tenSeconds = "0";
 let seconds = "0";
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
 
 // Data from Back-End
 let word = "apple";
 
+// Need to add the actual image paths and add images to static folder
+// Right now they aren't being seen
 // Images
 const pencil = new Image();
 pencil.src = "pencil.png";
@@ -49,39 +54,57 @@ var Ccnvs = coloursCanvas.getContext("2d");
 var toolsCanvas = document.getElementById("ToolsCanvas");
 var Tcnvs = toolsCanvas.getContext("2d");
 
-// Keep drawing pixels at the cursor
-function mouseHold() {
-    // Run drawPixel every 10ms
-    drawLoop = setInterval(drawPixel, 10, 5, 5, colour);
+// Start drawing when mouse is pressed
+function startDrawing(e) {
+    isDrawing = true;
+    let [canvasX, canvasY] = getCanvasCorners(drawingCanvas);
+    lastX = e.clientX - canvasX;
+    lastY = e.clientY - canvasY;
+}
+
+// Draw line to current position
+function draw(e) {
+    if (!isDrawing) return;
+    
+    let [canvasX, canvasY] = getCanvasCorners(drawingCanvas);
+    let currentX = e.clientX - canvasX;
+    let currentY = e.clientY - canvasY;
+    
+    Dcnvs.lineJoin = 'round';
+    Dcnvs.lineCap = 'round';
+    
+    if (mode === "pencil") {
+        Dcnvs.strokeStyle = colour;
+        Dcnvs.lineWidth = 5;
+        
+        Dcnvs.beginPath();
+        Dcnvs.moveTo(lastX, lastY);
+        Dcnvs.lineTo(currentX, currentY);
+        Dcnvs.stroke();
+    } else {
+        // Eraser mode
+        Dcnvs.strokeStyle = "white";
+        Dcnvs.lineWidth = 15;
+        
+        Dcnvs.beginPath();
+        Dcnvs.moveTo(lastX, lastY);
+        Dcnvs.lineTo(currentX, currentY);
+        Dcnvs.stroke();
+    }
+    
+    lastX = currentX;
+    lastY = currentY;
 }
 
 // Stop drawing
-function mouseRelease() {
-    clearInterval(drawLoop);
+function stopDrawing() {
+    isDrawing = false;
 }
 
 //Get coords of canvas topleft corner
 function getCanvasCorners(canvas) {
     let rect = canvas.getBoundingClientRect();
-    return [rect.left, rect.top]
-}
-
-// Draw pixel at mouse coords
-function drawPixel(width, height, colour) {
-    let [canvasX, canvasY] = getCanvasCorners(drawingCanvas);
-
-    // If current mode is pencil
-    if (mode == "pencil") {
-        Dcnvs.fillStyle = colour;
-        Dcnvs.fillRect(mouseX - canvasX - 10, mouseY - canvasY - 10, 5, 5);
-    }
-
-    // If current mode is eraser
-    else {
-        Dcnvs.fillStyle = "white";
-        Dcnvs.fillRect(mouseX - canvasX - 10, mouseY - canvasY - 10, 15, 15);
-    }
-    Dcnvs.stroke(); // Send data to back end, where to draw a pixel or to delete a pixel
+    return [rect.left, rect.top];
 }
 
 // Draw the Colour Choice Circles
@@ -99,38 +122,38 @@ function isInsideCircle(mouseX, mouseY) {
     let [canvasX, canvasY] = getCanvasCorners(coloursCanvas);
     // Run checkColour for each colour
     if (checkColour(mouseX - canvasX, mouseY - canvasY, 40)) {
-        colour = "red"
+        colour = "red";
     }
     else if (checkColour(mouseX - canvasX, mouseY - canvasY, 130)) {
-        colour = "orange"
+        colour = "orange";
     }
     else if (checkColour(mouseX - canvasX, mouseY - canvasY, 220)) {
-        colour = "yellow"
+        colour = "yellow";
     }
     else if (checkColour(mouseX - canvasX, mouseY - canvasY, 310)) {
-        colour = "green"
+        colour = "green";
     }
     else if (checkColour(mouseX - canvasX, mouseY - canvasY, 400)) {
-        colour = "blue"
+        colour = "blue";
     }
     else if (checkColour(mouseX - canvasX, mouseY - canvasY, 490)) {
-        colour = "purple"
+        colour = "purple";
     }
     else if (checkColour(mouseX - canvasX, mouseY - canvasY, 580)) {
-        colour = "pink"
+        colour = "pink";
     }
     else if (checkColour(mouseX - canvasX, mouseY - canvasY, 670)) {
-        colour = "brown"
+        colour = "brown";
     }
     else if (checkColour(mouseX - canvasX, mouseY - canvasY, 760)) {
-        colour = "black"
+        colour = "black";
     }
 }
 
 // Check if the click is in circle
 function checkColour(mouseX, mouseY, circleX) {
     const distance = Math.sqrt((mouseX - circleX) ** 2 + (mouseY - 40) ** 2); // Calculate distance to center of circle
-    return (distance <= 30) // True if mouse is in circle
+    return (distance <= 30); // True if mouse is in circle
 }
 
 // Add text to a canvas
@@ -143,18 +166,44 @@ function addText(cnvs, size, align, text, textX, textY) {
 
 // Runs when the Back Button is pressed (incomplete)
 function back() {
-    console.log("Back Button") // Send to back end that user has left the room
+    console.log("Back Button"); // Send to back end that user has left the room
+}
+
+// Highlight the selected tool
+function updateToolHighlight() {
+    // Clear the tools canvas
+    Tcnvs.clearRect(0, 0, 150, 75);
+    
+    // Draw both tools
+    Tcnvs.drawImage(pencil, 0, 0, 75, 75);
+    Tcnvs.drawImage(eraser, 75, 0, 75, 75);
+    
+    // Add highlight to the selected tool
+    Tcnvs.lineWidth = 4;
+    
+    if (mode === "pencil") {
+        // Highlight pencil
+        Tcnvs.strokeStyle = "#ff6bcb";
+        Tcnvs.strokeRect(2, 2, 71, 71);
+    } else {
+        // Highlight eraser
+        Tcnvs.strokeStyle = "#ff6bcb";
+        Tcnvs.strokeRect(77, 2, 71, 71);
+    }
 }
 
 // Runs when the pencil or eraser image is clicked
 function toolsClick(mouseX) {
     let [canvasX, canvasY] = getCanvasCorners(toolsCanvas);
     if (mouseX - canvasX <= 75) {
-        mode = "pencil"
+        mode = "pencil";
     }
     else {
-        mode = "eraser"
+        mode = "eraser";
     }
+    
+    // Update the visual highlight
+    updateToolHighlight();
 }
 
 // Update Timer
@@ -172,31 +221,28 @@ function runTimer() {
             if (minutes == "0") {
                 addText(TIcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 75, 50);
                 stopTimer();
-                console.log("Time's Up! The word was: " + word)
+                console.log("Time's Up! The word was: " + word);
             }
-
             else {
-                seconds = "9"
-                tenSeconds = "5"
-                minutes = parseInt(minutes) - 1
-                minutes = minutes.toString()
+                seconds = "9";
+                tenSeconds = "5";
+                minutes = parseInt(minutes) - 1;
+                minutes = minutes.toString();
     
                 addText(TIcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 75, 50);
             }
         }
-
         else {
-            seconds = "9"
-            tenSeconds = parseInt(tenSeconds) - 1
-            tenSeconds = tenSeconds.toString()
+            seconds = "9";
+            tenSeconds = parseInt(tenSeconds) - 1;
+            tenSeconds = tenSeconds.toString();
 
             addText(TIcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 75, 50);
         }
     }
-
     else {
-        seconds = parseInt(seconds) - 1
-        seconds = seconds.toString()
+        seconds = parseInt(seconds) - 1;
+        seconds = seconds.toString();
 
         addText(TIcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 75, 50);
     }
@@ -207,20 +253,27 @@ function stopTimer() {
     clearInterval(timer);
 }
 
-// Detect when the mouse clicks on coloursCanvas
-coloursCanvas.addEventListener("click", function() { isInsideCircle(mouseX, mouseY); });
-
-// Detect when the mouse is moved
-document.addEventListener("mousemove", function(event) {
-    mouseX = event.clientX; // Update global X coordinate
-    mouseY = event.clientY; // Update global Y coordinate
+// Event listeners
+coloursCanvas.addEventListener("click", function(e) { 
+    isInsideCircle(e.clientX, e.clientY); 
 });
 
-// Detect when mouse is held down or released
+document.addEventListener("mousemove", function(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
+
+// Drawing event listeners
+drawingCanvas.addEventListener("mousedown", startDrawing);
+drawingCanvas.addEventListener("mousemove", draw);
+document.addEventListener("mouseup", stopDrawing);
+document.addEventListener("mouseout", stopDrawing);
+
+// Back button event listener
 backCanvas.addEventListener("mouseup", back);
-drawingCanvas.addEventListener("mousedown", mouseHold);
-document.addEventListener("mouseup", mouseRelease);
-toolsCanvas.addEventListener("mouseup", function() { toolsClick(mouseX); });
+
+// Tools event listener
+toolsCanvas.addEventListener("mouseup", function(e) { toolsClick(e.clientX); });
 
 // Background for Back Canvas
 Bcnvs.fillStyle = "#f93cff";
@@ -228,15 +281,15 @@ Bcnvs.fillRect(0, 0, 150, 75);
 
 // Background for Word Canvas
 Wcnvs.fillStyle = "#ffd296";
-Wcnvs.fillRect(0, 0, 800, 75)
+Wcnvs.fillRect(0, 0, 800, 75);
 
 // Background for Timer Canvas
 TIcnvs.fillStyle = "#ffd296";
-TIcnvs.fillRect(0, 0, 150, 75)
+TIcnvs.fillRect(0, 0, 150, 75);
 
 // Background for Leaderboard Canvas
 Lcnvs.fillStyle = "#ffd296";
-Lcnvs.fillRect(0, 0, 150, 450)
+Lcnvs.fillRect(0, 0, 150, 450);
 
 // White Background for Drawing Canvas
 Dcnvs.fillStyle = "white";
@@ -244,35 +297,44 @@ Dcnvs.fillRect(0, 0, 800, 500);
 
 // Background for Chat Canvas
 CHcnvs.fillStyle = "#ffd296";
-CHcnvs.fillRect(0, 0, 150, 450)
-
-// Back Button Text
-addText(Bcnvs, "40", "center", "Back", 75, 50)
+CHcnvs.fillRect(0, 0, 150, 450);
 
 // When these images load, put them on the screen
 pencil.onload = function () {
-    Tcnvs.drawImage(pencil, 0, 0, 75, 75);
+    if (eraser.complete) {
+        updateToolHighlight(); // Only update when both images are loaded
+    } else {
+        Tcnvs.drawImage(pencil, 0, 0, 75, 75);
+    }
 };
+
 eraser.onload = function () {
-    Tcnvs.drawImage(eraser, 75, 0, 75, 75);
+    if (pencil.complete) {
+        updateToolHighlight(); // Only update when both images are loaded
+    } else {
+        Tcnvs.drawImage(eraser, 75, 0, 75, 75);
+    }
 };
 
 // Draw the colour circles to the screen
-drawCircle(40, "red")
-drawCircle(130, "orange")
-drawCircle(220, "yellow")
-drawCircle(310, "green")
-drawCircle(400, "blue")
-drawCircle(490, "purple")
-drawCircle(580, "pink")
-drawCircle(670, "brown")
-drawCircle(760, "black")
+drawCircle(40, "red");
+drawCircle(130, "orange");
+drawCircle(220, "yellow");
+drawCircle(310, "green");
+drawCircle(400, "blue");
+drawCircle(490, "purple");
+drawCircle(580, "pink");
+drawCircle(670, "brown");
+drawCircle(760, "black");
 
 // Display the word at the top
-addText(Wcnvs, "50", "center", word, 400, 50)
+addText(Wcnvs, "50", "center", word, 400, 50);
+
+// Back Button Text
+addText(Bcnvs, "40", "center", "Back", 75, 50);
 
 // Timer Text 
 addText(TIcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 75, 50);
 
 // Start Timer
-timer = setInterval(runTimer, 1000)
+timer = setInterval(runTimer, 1000);
