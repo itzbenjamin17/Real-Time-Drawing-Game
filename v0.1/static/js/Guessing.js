284f0db94baf67ed5805e190376f75778b5fbeb3
John C. Haboc <john.haboc@student.manchester.ac.uk>
2025-03-18 22:07:06 +0000

    Implemented the back button.

--- v0.1/static/js/Guessing.js
+++ v0.1/static/js/Guessing.js
@@ -91,7 +91,8 @@ function addText(cnvs, size, align, text, textX, textY) {
 
 // Runs when the Back Button is pressed (**incomplete**)
 function back() {
-    console.log("Back Button") // Send to back end that user has left the room
+    console.log("Back Button"); // Send to back end that user has left the room
+    window.location.href = '/';
 }
 
 // Update Timer
