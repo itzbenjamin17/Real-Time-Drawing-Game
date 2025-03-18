762ff5e9447bc46602edf9b657a59fe4d2d57b60
John C. Haboc <john.haboc@student.manchester.ac.uk>
2025-03-18 23:24:07 +0000

    Reverting.

--- v0.1/static/js/Guessing.js
+++ v0.1/static/js/Guessing.js
@@ -1,8 +1,8 @@
 // ---Variables---
 let mouseX = 0;
 let mouseY = 0;
-let minutes = "0";
-let tenSeconds = "1";
+let minutes = "3";
+let tenSeconds = "0";
 let seconds = "0";
 
 // ---Data from Back-End---
@@ -24,6 +24,8 @@ let score4 = 239999;
 let username5 = "shelllll";
 let score5 = 2399999;
 
+let numOfPlayers = 5;
+
 // ---Define the Canvases---
 // Back Canvas
 var backCanvas = document.getElementById("BackCanvas");
@@ -41,19 +43,7 @@ var Tcnvs = timerCanvas.getContext("2d");
 var leaderboardCanvas = document.getElementById("LeaderboardCanvas");
 var Lcnvs = leaderboardCanvas.getContext("2d");
 
- // Create Drawing Canvas
- var drawingCanvas = document.getElementById("DrawingImage");
-
-// Chat Canvas
-var chatCanvas = document.getElementById("ChatCanvas");
-var CHcnvs = chatCanvas.getContext("2d");
-
-// Create Guessing Canvas
-var guessingCanvas = document.getElementById("GuessingCanvas");
-var Gcnvs = guessingCanvas.getContext("2d");
-
-// Text Area
-let textArea = document.getElementById("Guess");
+var drawingCanvas = document.getElementById("DrawingImage");
 
 // ---Functions---
 
@@ -66,7 +56,7 @@ function getCanvasCorners(canvas) {
 // Display word at Word Canvas
 function displayWord(word, guessed) {
     if (guessed) { // If user has guessed the word
-        addText(Wcnvs, "50", "center", word, 400, 45)
+        addText(Wcnvs, "50", "center", word, 350, 55)
     }
     else { // User has not guessed the word
         // Get underscores for each letter in word
@@ -77,7 +67,7 @@ function displayWord(word, guessed) {
         }
         displayedWord = displayedWord.slice(0, -1);
         
-        addText(Wcnvs, "50", "center", displayedWord, 400, 45)
+        addText(Wcnvs, "50", "center", displayedWord, 350, 55)
     }
 }
 
@@ -91,33 +81,27 @@ function addText(cnvs, size, align, text, textX, textY) {
 
 // Runs when the Back Button is pressed (**incomplete**)
 function back() {
-    console.log("Back Button"); // Send to back end that user has left the room
+    console.log("Back Button") // Send to back end that user has left the room
     window.location.href = '/';
 }
 
 // Update Timer
 function runTimer() {
     // Clear Tcnvs
-    Tcnvs.clearRect(0, 0, 150, 75);
+    Tcnvs.clearRect(0, 0, 250, 75);
 
     // Background
     Tcnvs.fillStyle = "#ffd296";
-    Tcnvs.fillRect(0, 0, 150, 75);
+    Tcnvs.fillRect(0, 0, 250, 75);
 
     // update timer
     if (seconds == "0") {
         if (tenSeconds == "0") {
             if (minutes == "0") {
-                addText(Tcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 75, 50);
+                addText(Tcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 125, 50);
                 stopTimer();
                 console.log("Time's Up! The word was: " + word)
                 // Need to add logic to end the round and move on to the next
-                
-                // Store data (username, score, current round, etc.)
-
-                // Redirect to new round
-                socketio.emit("new_round");
-
             }
 
             else {
@@ -126,7 +110,7 @@ function runTimer() {
                 minutes = parseInt(minutes) - 1
                 minutes = minutes.toString()
     
-                addText(Tcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 75, 50);
+                addText(Tcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 125, 50);
             }
         }
 
@@ -135,7 +119,7 @@ function runTimer() {
             tenSeconds = parseInt(tenSeconds) - 1
             tenSeconds = tenSeconds.toString()
 
-            addText(Tcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 75, 50);
+            addText(Tcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 125, 50);
         }
     }
 
@@ -143,7 +127,7 @@ function runTimer() {
         seconds = parseInt(seconds) - 1
         seconds = seconds.toString()
 
-        addText(Tcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 75, 50);
+        addText(Tcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 125, 50);
     }
 }
 
@@ -152,36 +136,16 @@ function stopTimer() {
     clearInterval(timer);
 }
 
-// Resize the text area according to when the window is resized
-function adjustTextArea() {
-    let canvas = document.getElementById("GuessingCanvas");
-
-    // Adjust size based on canvas size
-    textArea.style.width = canvas.width * 0.965 + "px";
-    textArea.style.height = canvas.height * 0.655 + "px";
-
-    // Adjust position based on canvas position
-    let [canvasX, canvasY] = getCanvasCorners(guessingCanvas)
-    textArea.style.left = canvasX + canvas.width * 0.025 + "px";
-    textArea.style.top = canvasY + canvas.height * 0.25 + "px";
-}
-
-// Check the guess
-function makeGuess() {
-    if (textArea.value == word) {
-        console.log("Correct!") // Send to back end that user has guessed correctly
-        Wcnvs.clearRect(0, 0, 800, 75)
-        
-        // Background for Word Canvas
-        Wcnvs.fillStyle = "#ffd296";
-        Wcnvs.fillRect(0, 0, 800, 75)
-        
-        // Display the word at the top
-        displayWord(word, true);
-    }
-    else {
-        console.log("Wrong. Guess again!") // Send to back end user's guess
-    }
+// Player has guesssed correctly
+function correctGuess() {
+    Wcnvs.clearRect(0, 0, 700, 75)
+    
+    // Background for Word Canvas
+    Wcnvs.fillStyle = "#ffd296";
+    Wcnvs.fillRect(0, 0, 700, 75)
+    
+    // Display the word at the top
+    displayWord(word, true);
 }
 
 // Need logic that checks if all guessing players have guessed correctly
@@ -189,106 +153,77 @@ function makeGuess() {
 function loadLeaderboard() {
     // Load player list
 
-    // Draw the black outline
+    // 1st Place
     Lcnvs.strokeStyle = "black";
     Lcnvs.lineWidth = 2;
-    Lcnvs.strokeRect(10, 10, 130, 75); 
-    Lcnvs.strokeRect(10, 95, 130, 75);
-    Lcnvs.strokeRect(10, 185, 130, 75);
-    Lcnvs.strokeRect(10, 275, 130, 75);
-    Lcnvs.strokeRect(10, 365, 130, 75);
+    Lcnvs.strokeRect(10, 10, 130, 75);
 
-    // Draw the white background
     Lcnvs.fillStyle = "white";
     Lcnvs.fillRect(10, 10, 130, 75);
-    Lcnvs.fillRect(10, 95, 130, 75);
-    Lcnvs.fillRect(10, 185, 130, 75);
-    Lcnvs.fillRect(10, 275, 130, 75);
-    Lcnvs.fillRect(10, 365, 130, 75);
 
-    // Add the texts
     addText(Lcnvs, "20", "center", "1st", 75, 35);
     addText(Lcnvs, "20", "center", username1, 75, 55);
     addText(Lcnvs, "20", "center", score1.toString(), 75, 75);
 
-    addText(Lcnvs, "20", "center", "2nd", 75, 120);
-    addText(Lcnvs, "20", "center", username2, 75, 140);
-    addText(Lcnvs, "20", "center", score2.toString(), 75, 160);
+    if (numOfPlayers >= 2) {
+        // 2nd Place
+        Lcnvs.strokeStyle = "black";
+        Lcnvs.strokeRect(10, 95, 130, 75);
 
-    addText(Lcnvs, "20", "center", "3rd", 75, 210);
-    addText(Lcnvs, "20", "center", username3, 75, 230);
-    addText(Lcnvs, "20", "center", score3.toString(), 75, 250);
+        Lcnvs.fillStyle = "white";
+        Lcnvs.fillRect(10, 95, 130, 75);
 
-    addText(Lcnvs, "20", "center", "4th", 75, 300);
-    addText(Lcnvs, "20", "center", username4, 75, 320);
-    addText(Lcnvs, "20", "center", score4.toString(), 75, 340);
+        addText(Lcnvs, "20", "center", "2nd", 75, 120);
+        addText(Lcnvs, "20", "center", username2, 75, 140);
+        addText(Lcnvs, "20", "center", score2.toString(), 75, 160);
 
-    addText(Lcnvs, "20", "center", "5th", 75, 390);
-    addText(Lcnvs, "20", "center", username5, 75, 410);
-    addText(Lcnvs, "20", "center", score5.toString(), 75, 430);
+        if (numOfPlayers >= 3) {
+            // 3rd Place
+            Lcnvs.strokeStyle = "black";
+            Lcnvs.strokeRect(10, 185, 130, 75);
+
+            Lcnvs.fillStyle = "white";
+            Lcnvs.fillRect(10, 185, 130, 75);
+
+            addText(Lcnvs, "20", "center", "3rd", 75, 210);
+            addText(Lcnvs, "20", "center", username3, 75, 230);
+            addText(Lcnvs, "20", "center", score3.toString(), 75, 250);
+
+            if (numOfPlayers >= 4) {
+                // 4th Place
+                Lcnvs.strokeStyle = "black";
+                Lcnvs.strokeRect(10, 275, 130, 75);
+
+                Lcnvs.fillStyle = "white";
+                Lcnvs.fillRect(10, 275, 130, 75);
+
+                addText(Lcnvs, "20", "center", "4th", 75, 300);
+                addText(Lcnvs, "20", "center", username4, 75, 320);
+                addText(Lcnvs, "20", "center", score4.toString(), 75, 340);
+
+                if (numOfPlayers >= 5) {
+                    // 5th Place
+                    Lcnvs.strokeStyle = "black";
+                    Lcnvs.strokeRect(10, 365, 130, 75);
+
+                    Lcnvs.fillStyle = "white";
+                    Lcnvs.fillRect(10, 365, 130, 75);
+
+                    addText(Lcnvs, "20", "center", "5th", 75, 390);
+                    addText(Lcnvs, "20", "center", username5, 75, 410);
+                    addText(Lcnvs, "20", "center", score5.toString(), 75, 430);
+                }
+            }
+        }
+    }
 }
 
-// ---Event Listeners---
-
-// Detect when the mouse is moved
-document.addEventListener("mousemove", function(event) {
-    mouseX = event.clientX; // Update global X coordinate
-    mouseY = event.clientY; // Update global Y coordinate
-});
-
-// Detect when mouse is held down or released
-backCanvas.addEventListener("mouseup", back);
-
-// Detect when the window is resized
-window.addEventListener("resize", adjustTextArea)
-
-// Detect when the enter key is released when the text area is active
-textArea.addEventListener("keyup", function(event) { 
-    if (event.key === "Enter") {
-        makeGuess()
-    }
-})
-
-// ---Drawing to the screen---
-
-// Background for Back Canvas
-Bcnvs.fillStyle = "#f93cff";
-Bcnvs.fillRect(0, 0, 150, 75);
-
-// Background for Word Canvas
-Wcnvs.fillStyle = "#ffd296";
-Wcnvs.fillRect(0, 0, 800, 75)
-
-// Background for Timer Canvas
-Tcnvs.fillStyle = "#ffd296";
-Tcnvs.fillRect(0, 0, 150, 75)
-
-// Background for Leaderboard Canvas
-Lcnvs.fillStyle = "#ffd296";
-Lcnvs.fillRect(0, 0, 150, 450)
-
-// Background for Chat Canvas
-CHcnvs.fillStyle = "#ffd296";
-CHcnvs.fillRect(0, 0, 150, 450)
-
-// Background for Guessing Canvas
-Gcnvs.fillStyle = "#ffd296";
-Gcnvs.fillRect(0, 0, 800, 75)
-
-// Back Button Text
-addText(Bcnvs, "40", "center", "Back", 75, 50);
-
-// Timer Text 
-addText(Tcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 75, 50);
-
-loadLeaderboard();
-
-// Set initial size when the sreen first loads
-window.onload = adjustTextArea;
 
 // ---Socketio---
 
 var socketio = io();
+
+const messages = document.getElementById("messages");
 
 window.onload = function() {
     document.getElementById('overlay').style.display = 'block';
@@ -309,6 +244,12 @@ socketio.on('wordSelected', (wordToGuess) => {
     word = wordToGuess;
     displayWord(word, false);
     timer = setInterval(runTimer, 1000);
+});
+
+socketio.on("message", (data) => {
+    console.log("Received Message:", data);
+    createMessage(data.name, data.message);
+    console.log("Still connected...")
 });
 
 socketio.on('redirect', (url) => {
@@ -337,3 +278,71 @@ socketio.on("display_drawing", (data) => {
     console.error("Failed to load image.");
     };
 });
+
+
+// Chat
+const sendMessage = () => {
+    const message = document.getElementById("message"); // fetch message
+    if (message.value == "") return; // if empty do nothing
+    socketio.emit("message", {data: message.value}); // send to other players
+    if (message.value == word) {
+        socketio.emit("message", {data: "You've Guessed Correctly!"});
+        correctGuess();
+    }
+    message.value = ""; // empties box
+};
+
+const createMessage = (name, msg) => {
+    const content = `
+    <div class="text">
+        <span>
+            <strong>${name}</strong>: ${msg}
+        </span>
+    </div>
+    `;
+    messages.innerHTML += content;
+    messages.scrollTop = messages.scrollHeight;
+};
+
+// ---Event Listeners---
+
+// Detect when the mouse is moved
+document.addEventListener("mousemove", function(event) {
+    mouseX = event.clientX; // Update global X coordinate
+    mouseY = event.clientY; // Update global Y coordinate
+});
+
+// Detect when mouse is held down or released
+backCanvas.addEventListener("mouseup", back);
+
+// Chat or Guessing
+document.getElementById("send-btn").addEventListener("click", sendMessage);
+document.getElementById("message").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") sendMessage();
+});
+
+// ---Drawing to the screen---
+
+// Background for Back Canvas
+Bcnvs.fillStyle = "#f93cff";
+Bcnvs.fillRect(0, 0, 150, 75);
+
+// Background for Word Canvas
+Wcnvs.fillStyle = "#ffd296";
+Wcnvs.fillRect(0, 0, 700, 75)
+
+// Background for Timer Canvas
+Tcnvs.fillStyle = "#ffd296";
+Tcnvs.fillRect(0, 0, 250, 75)
+
+// Background for Leaderboard Canvas
+Lcnvs.fillStyle = "#ffd296";
+Lcnvs.fillRect(0, 0, 150, 450)
+
+// Back Button Text
+addText(Bcnvs, "40", "center", "Back", 75, 50);
+
+// Timer Text 
+addText(Tcnvs, "40", "center", minutes + ":" + tenSeconds + seconds, 125, 50);
+
+loadLeaderboard();
